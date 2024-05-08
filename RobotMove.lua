9ad29ce71402145b4e8a9c6953c37b05a48f46f6
ljhljll <1356008591@qq.com>
2024-05-08 18:06:15 +0800

    修改机器人移动处理

--- RobotMove.lua
+++ RobotMove.lua
@@ -2,11 +2,17 @@ local component = require("component")
 local robot = require("robot")
 local sides = require("sides")
 
-local robotMove = {}
+---向前走一步(如遇到障碍物或没电则反复重试)
+local function tryForward()
+    local forwardFlag
+    repeat
+        forwardFlag = robot.forward()
+    until forwardFlag
+end
 
 ---向左平移 默认移动1步
 ---@param step integer @comment 平移{step}步
-function robotMove.leftWard(step)
+local function leftWard(step)
     step = step or 1
     robot.turnLeft()
     for i = 1, step, 1 do
@@ -17,13 +23,17 @@ end
 
 ---向右平移 默认移动1步
 ---@param step integer @comment 平移{step}步
-function robotMove.rightWard(step)
+local function rightWard(step)
     step = step or 1
     robot.turnRight()
     for i = 1, step, 1 do
-        robot.forward()
+        tryForward()
     end
     robot.turnLeft()
 end
 
-return robotMove
+return {
+    leftWard = leftWard,
+    rightWard = rightWard,
+    tryForward = tryForward
+}
